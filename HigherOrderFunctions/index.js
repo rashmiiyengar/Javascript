83a522e0208937ad2873b8501a054121b4b3fb06
Rashmi <rashmiiyengar255@gmail.com>
2023-10-13 05:36:03 +0530

    Higher Orde Functions

---
+++ HigherOrderFunctions/index.js
@@ -0,0 +1,38 @@
+const radius = [3,4,7,8];
+
+const area =(radius)=>{
+    return Math.PI * radius *radius ;
+}//Callback Functions
+
+const circumference=(radius)=>{
+    return 2*Math.PI *radius;
+}
+
+const diameter =(radius)=>{
+    return 2*radius;
+}
+const calculate = (radius,logic)=>{
+const output =[];
+for(let i=0;i<radius.length;i++){
+
+    output.push(logic(radius[i]));
+}
+return output;
+}//Higher Order Functions//map func replication
+
+console.log(calculate(radius,area));
+console.log(calculate(radius,circumference));
+console.log(calculate(radius,diameter));
+
+console.log(radius.map(diameter))
+
+Array.prototype.calculateMap = (logic)=>{
+    const res =[];
+    for(let i=0;i<this.length;i++){
+    
+        res.push(logic(this[i]));
+    }
+    return res;
+    }
+
+    console.log(radius.calculateMap(diameter))
